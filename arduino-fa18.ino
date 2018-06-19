9f18a6e7ca9dd4a846210ef27da81ed5cc1cfb71
Connor Worley <connorbworley@gmail.com>
2018-06-19 13:53:54 -0700

    Basic PS4BT code

--- arduino-fa18.ino
+++ arduino-fa18.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <PS4BT.h>
+#include <SPI.h>
 
+USB Usb;
+BTD Btd(&Usb);
+PS4BT PS4(&Btd);
+
+void setup() {
+  Serial.begin(115200);
+#if !defined(__MIPSEL__)
+  while(!Serial); // wait for serial port to connect
+#endif
+  if(Usb.Init() == -1) {
+    Serial.print(F("USB host shield failed to start\n"));
+    while(1); // halt program
+  }
+  Serial.print(F("USB host shield started\n"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Usb.Task();
 
+  if(PS4.connected()) {
+    Serial.print(F("\nLeft X: "));
+    Serial.print(PS4.getAnalogHat(LeftHatX));
+  }
 }
